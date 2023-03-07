3030b085cb62d8e7e89e7db888067beab8275e53
Street1g <nthng717@gmail.com>
2023-03-07 22:44:35 +0300

    fix src/even.js

--- src/even.js
+++ src/even.js
@@ -15,16 +15,15 @@ const randomNumber = () => {
 };
 
 const checkEven = () => {
-  const answerArr = ['yes', 'no'];
   const playerName = greeting();
   for (let i = 0; i < 3; i += 1) {
     const number = randomNumber();
+    const correctAnswer = (number % 2 === 0 ? 'yes' : 'no');
     const playerAnswer = readlineSync.question(`Question: ${number}\nYour answer: `);
-    const reverseAnswer = (playerAnswer === answerArr[0] ? answerArr[1] : answerArr[0]);
-    if ((number % 2 === 0 && playerAnswer === answerArr[0]) || (number % 2 !== 0 && playerAnswer === answerArr[1])) {
+    if (playerAnswer === correctAnswer) {
       console.log('Correct!');
     } else {
-      console.log(`${playerAnswer} is wrong answer ;(. Correct answer was ${reverseAnswer}\nLet's try again, ${playerName}!`);
+      console.log(`${playerAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}\nLet's try again, ${playerName}!`);
       return;
     }
   }
