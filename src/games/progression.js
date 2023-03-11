3277dcd28e71d0032d6b72379cddfc7883537fc0
Street1g <nthng717@gmail.com>
2023-03-11 21:23:05 +0300

    fix games/progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,6 +1,7 @@
 import brainGame from '../index.js';
 
 const rules = 'What number is missing in the progression?';
+const progresionLength = 10;
 
 const randomNumber = (min, max) => {
   const number = Math.floor(Math.random() * (max - min) + min);
@@ -11,7 +12,7 @@ const progressionQuestion = () => {
   const progression = [];
   let progressionNumber = randomNumber(0, 100);
   const progressionStep = randomNumber(5, 10);
-  for (let i = 0; i < 10; i += 1) {
+  for (let i = 0; i < progresionLength; i += 1) {
     progression.push(progressionNumber);
     progressionNumber += progressionStep;
   }
