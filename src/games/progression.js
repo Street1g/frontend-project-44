5a038d673a24f7efc9f66f3e714f8e0dd13b5a51
Street1g <nthng717@gmail.com>
2023-03-11 21:42:55 +0300

    fix games/progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -11,7 +11,7 @@ const randomNumber = (min, max) => {
 const progressionQuestion = () => {
   const progression = [];
   let progressionNumber = randomNumber(0, 100);
-  const progressionStep = randomNumber(5, 10);
+  const progressionStep = randomNumber(1, 10);
   for (let i = 0; i < progresionLength; i += 1) {
     progression.push(progressionNumber);
     progressionNumber += progressionStep;
